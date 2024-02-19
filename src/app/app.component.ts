d8c48277506d53e955546c0a96e01646085f48fc
Simon Yuen <simonyuen1999@hotmail.com>
2024-02-19 12:48:49 -0500

    Add ngFor to traverse componets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Type } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminBioComponent } from './admin-bio/admin-bio.component';
 import { StandardBioComponent } from './standard-bio/standard-bio.component';
@@ -8,18 +8,31 @@ import { StandardBioComponent } from './standard-bio/standard-bio.component';
   standalone: true,
   imports: [CommonModule],
   template: `
-   <ng-container *ngComponentOutlet="component"></ng-container>
-   <button (click)="toggleComponent()">Toggle Component</button>
+  <h1>{{ title }}</h1>
+  
+  <hr>
+  <ng-container *ngFor="let a_comp of components">
+      <ng-container *ngComponentOutlet="a_comp"></ng-container>
+    </ng-container>
+  <hr>
+
+  <button (click)="toggleComponent()">Toggle Component</button>
+  <ng-container *ngComponentOutlet="flip_comp"></ng-container> 
+  <hr>
+
+  <small>__END__</small>
   `,
 })
 export class AppComponent {
   title = 'dyn-comp-app';
 
+  components: Type<any>[] = [AdminBioComponent, StandardBioComponent];
+
   isAdmin = false;
-  component = StandardBioComponent;
+  flip_comp = StandardBioComponent;
 
   toggleComponent() {
     this.isAdmin = !this.isAdmin;
-    this.component = this.isAdmin ? AdminBioComponent : StandardBioComponent;
+    this.flip_comp = this.isAdmin ? AdminBioComponent : StandardBioComponent;
   }
 }
